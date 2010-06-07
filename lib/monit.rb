9675c9ecb5e8dc1521364c3d82e452c88f853363
Oleh Khomey <varyform@gmail.com>
2010-06-07 19:47:42 +0300

    Changed owner back to root

--- lib/monit.rb
+++ lib/monit.rb
@@ -15,8 +15,8 @@ module Monit
     file '/etc/monit/monitrc',
       :content => template(File.join(File.dirname(__FILE__), '..', 'templates', 'monitrc'), binding),
       :mode => '600',
-      :owner => configuration[:user],
-      :group => configuration[:group] || configuration[:user],
+      :owner => 'root',
+      :group => 'root',
       :require => package('monit'),
       :before => service('monit'),
       :notify => service('monit')
@@ -24,28 +24,28 @@ module Monit
     file '/etc/monit/conf.d',
       :ensure => :directory,
       :mode   => '755',
-      :owner => configuration[:user],
-      :group => configuration[:group] || configuration[:user],
+      :owner =>  'root',
+      :group =>  'root',
       :before => service("monit")
 
     file '/etc/monit/conf.d/apache',
       :content => template(File.join(File.dirname(__FILE__), '..', 'templates', 'apache'), binding),
       :mode => '600',
-      :owner => configuration[:user],
-      :group => configuration[:group] || configuration[:user],
+      :owner =>  'root',
+      :group =>  'root',
       :require => file('/etc/monit/conf.d')
 
     file '/etc/default/monit',
       :content => template(File.join(File.dirname(__FILE__), '..', 'templates', 'startup')),
       :mode => '644',
-      :owner => configuration[:user],
-      :group => configuration[:group] || configuration[:user],
+      :owner =>  'root',
+      :group =>  'root',
       :before => service("monit")
 
     file '/etc/init.d/monit',
       :mode => '755',
-      :owner => configuration[:user],
-      :group => configuration[:group] || configuration[:user],
+      :owner =>  'root',
+      :group =>  'root',
       :before => service("monit")
 
     exec 'restart_monit',
